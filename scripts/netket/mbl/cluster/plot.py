6a7c02eee17b534c08a8d621fd22446dd3734634
Archie Hewitt <hewitt.archie@yandex.ru>
2021-09-02 14:12:20 +0300

    added xlsx saving

--- scripts/netket/mbl/cluster/plot.py
+++ scripts/netket/mbl/cluster/plot.py
@@ -29,6 +29,8 @@ n_iter = 1000
 target_seed = seed_start
 target_iteration = n_iter
 
+metric = 'norm_rho_diff'
+
 for W_id, W in enumerate(Ws):
     print(f"W={W:0.4f}")
 
@@ -45,7 +47,7 @@ for W_id, W in enumerate(Ws):
     else:
         df_all = df_all.merge(curr_df, how='inner', left_index=True, right_index=True)
 
-columns = [f"{x}_{target_seed:d}_W({W:0.4f})" for x in ['norm_rho_diff'] for W in Ws]
+columns = [f"{x}_{target_seed:d}_W({W:0.4f})" for x in [metric] for W in Ws]
 norm_rho_diffs = [df_all.loc[target_iteration, col] for col in columns]
 
 path_save = path + '/plot/ndm_vs_exact'
@@ -70,4 +72,5 @@ fig.add_trace(
 )
 add_layout(fig, r"$W$", r"$\left\Vert \rho^{\text{exact}} - \rho^{\text{neural}} \right\Vert$",  "")
 fig.update_layout({'colorway': ['red', 'blue', "red", "red"]})
-save_figure(fig, f"{path_save}/NDM({alpha:d}_{beta:d}_{n_samples:d})_H(var_{U:0.4f}_{J:0.4f})_D({diss_type:d}_{diss_gamma:0.4f})_seed({target_seed})_it({target_iteration})")
+save_figure(fig, f"{path_save}/{metric}_NDM({alpha:d}_{beta:d}_{n_samples:d})_H(var_{U:0.4f}_{J:0.4f})_D({diss_type:d}_{diss_gamma:0.4f})_seed({target_seed})_it({target_iteration})")
+df_all.to_excel(f"{path_save}/{metric}_NDM({alpha:d}_{beta:d}_{n_samples:d})_H(var_{U:0.4f}_{J:0.4f})_D({diss_type:d}_{diss_gamma:0.4f}).xlsx", index=True)
