91aaf6927e9f5b6f0fbdc9f361a9c106ccbe415c
Archie Hewitt <hewitt.archie@yandex.ru>
2021-10-14 02:10:00 +0300

    plots for les

--- scripts/plot/integrable/les.py
+++ scripts/plot/integrable/les.py
@@ -6,21 +6,24 @@ from scripts.plot.routines.violin import add_violin_trace
 import os
 import plotly.graph_objects as go
 
-path = f"E:/YandexDisk/Work/os_lnd/draft/mbl/2/figures/lndham/lambda/100_100"
 
-N = 100
+path = f"E:/YandexDisk/Work/os_lnd/draft/mbl/2/figures/integrable/lambda"
+
+N = 7
+tau = 1
+k = -1
 alpha = 0.5
 n_seeds = 50
 T = 1.0
 
-lpn_type = -1 # energy
-lpn_log_deltas = [-6.0, -5.0, -4.0, -3.0]
-Ts = [1, 2]
+lpn_type = 0
+lpn_log_deltas = [-6.0, -5.0, -4.0]
+Ts = [1]
 
 vio = go.Figure()
 for T in Ts:
     for log_delta in lpn_log_deltas:
-        fn = f"{path}/lambda_N({N})_numSeeds({n_seeds})_alpha({alpha:0.4f})_T({T:0.4f})_lpn({lpn_type}_{log_delta:0.4f}).csv"
+        fn = f"{path}/lambda_N({N})_numSeeds({n_seeds})_tau({tau:d})_k({k:d})_T({T:0.4f})_lpn({lpn_type}_{log_delta:0.4f}).csv"
         les = pd.read_csv(fn, header=None)
         les['mean'] = les.mean(axis=1)
         les['period'] = fr"Period={T}"
@@ -33,5 +36,6 @@ for T in Ts:
 add_layout(vio, "", fr"$\lambda$", f"")
 vio.update_layout({'colorway': ['blue', 'red', 'green', 'orange']})
 save_figure(vio, f"{path}/lambda_N({N})_numSeeds({n_seeds})_alpha({alpha:0.4f})_lpn({lpn_type})_vio")
+
 
 
--- scripts/plot/routines/violin.py
+++ scripts/plot/routines/violin.py
@@ -1,13 +1,30 @@
 import plotly.graph_objects as go
 
 
-def add_violin_trace(fig, y, name):
-    showlegend = False if name == "" else True
-    fig.add_trace(
-        go.Violin(
-            y=y,
-            name=name,
-            box_visible=True,
-            showlegend=showlegend,
+def add_violin_trace(fig, y, name, x=None, showlegend=True):
+
+    if x is not None:
+        fig.add_trace(
+            go.Violin(
+                x=x,
+                y=y,
+                name=name,
+                box_visible=True,
+                showlegend=showlegend,
+                meanline_visible=True,
+                legendgroup=name,
+                scalegroup=name
+            )
         )
-    )
+    else:
+        fig.add_trace(
+            go.Violin(
+                y=y,
+                name=name,
+                box_visible=True,
+                showlegend=showlegend,
+                meanline_visible=True,
+                legendgroup=name,
+                scalegroup=name
+            )
+        )
